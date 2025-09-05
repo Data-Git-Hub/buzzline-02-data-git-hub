cbc844a754437e59f17117391e0f48aa0859ddc2
Data-Git-Hub <s576977@nwmissouri.edu>
2025-09-05 17:35:59 -0500

    P2.2 - 0.4 - Modify utils_consumer.py, project_log.log: fixing issues with kafka server

--- utils/utils_consumer.py
+++ utils/utils_consumer.py
@@ -4,53 +4,41 @@ utils_consumer.py - common functions used by consumers.
 Consumers subscribe to a topic and read messages from the Kafka topic.
 """
 
-#####################################
-# Import Modules
-#####################################
+# ============================
+# Imports
+# ============================
 
-
-# Import external packages
 from kafka import KafkaConsumer
-
-# Import functions from local modules
 from utils.utils_logger import logger
-from .utils_producer import get_kafka_broker_address
+from .utils_producer import get_kafka_broker_address, get_kafka_api_version
 
-
-#####################################
-# Helper Functions
-#####################################
-
+# ============================
+# Factory
+# ============================
 
 def create_kafka_consumer(
-    topic_provided: str = None,
-    group_id_provided: str = None,
+    topic_provided: str | None = None,
+    group_id_provided: str | None = None,
     value_deserializer_provided=None,
 ):
     """
     Create and return a Kafka consumer instance.
-
-    Args:
-        topic_provided (str): The Kafka topic to subscribe to. Defaults to the environment variable or default.
-        group_id_provided (str): The consumer group ID. Defaults to the environment variable or default.
-        value_deserializer_provided (callable, optional): Function to deserialize message values.
-
-    Returns:
-        KafkaConsumer: Configured Kafka consumer instance.
     """
     kafka_broker = get_kafka_broker_address()
+    api_version = get_kafka_api_version()
     topic = topic_provided
     consumer_group_id = group_id_provided or "test_group"
-    logger.info(f"Creating Kafka consumer. Topic='{topic}' and group ID='{group_id_provided}'.")
-    logger.debug(f"Kafka broker: {kafka_broker}")
+
+    logger.info(f"Creating Kafka consumer. Topic='{topic}' and group ID='{consumer_group_id}'.")
+    logger.debug(f"Kafka broker: {kafka_broker} | API version: {api_version}")
 
     try:
         consumer = KafkaConsumer(
             topic,
             group_id=consumer_group_id,
-            value_deserializer=value_deserializer_provided
-            or (lambda x: x.decode("utf-8")),
+            value_deserializer=value_deserializer_provided or (lambda x: x.decode("utf-8")),
             bootstrap_servers=kafka_broker,
+            api_version=api_version,
             auto_offset_reset="earliest",
             enable_auto_commit=True,
         )
